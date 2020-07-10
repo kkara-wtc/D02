5134c56cd05bd2a99d90d485490645b00aee168b
kkarac5r3s1Jhb <67952511+kkarac5r3s1Jhb@users.noreply.github.com>
2020-07-10 10:08:19 +0200

    combination

--- ex04/ft_print_comb.c
+++ ex04/ft_print_comb.c
@@ -13,17 +13,21 @@
 
 void ft_putchar(char c);
 
-void ft_print_comb(void) {
+void ft_print_comb(void) 
+{
 	int number[3];
 
     number[0] = '0';
-	while (number[0] <= '9'){
+	while (number[0] <= '9') 
+	{
 
 		    number[1] = number[0] + 1;
-			while (number[1] <= '9'){
+			while (number[1] <= '9') 
+			{
 
 				    number[2] = number[1] + 1;
-					while (number[2] <= '9'){
+					while (number[2] <= '9')
+					{
 
 						ft_putchar(number[0]);
 						ft_putchar(number[1]);
@@ -32,8 +36,11 @@ void ft_print_comb(void) {
 						if (number[0] != 7 && number[1] != 8 && number[2] != 9)
 						{	ft_putchar(',');
 							ft_putchar(' ');
-							}
-						number[2]++; }
-				number[1]++; }
-		number[0]++; }
+						}
+						number[2]++; 
+					}
+				number[1]++; 
+			}
+		number[0]++;
+	}
 }
