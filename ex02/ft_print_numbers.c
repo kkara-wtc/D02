c2f0c34d66f0b8545c62719108fce760154fb9ff
kkarac5r3s1Jhb <67952511+kkarac5r3s1Jhb@users.noreply.github.com>
2020-07-10 09:57:53 +0200

    print numbers

--- ex02/ft_print_numbers.c
+++ ex02/ft_print_numbers.c
@@ -13,12 +13,16 @@
 
 void ft_putchar(char c);
 
-void ft_print_numbers(void) {
-    char numbers;
+void ft_print_numbers(void) 
+{
+	char numbers;
+	
 	numbers = '0';
 
-	while(numbers <='9'){
+	while(numbers <='9') 
+	{
 		ft_putchar(numbers);
-		numbers ++; }
+		numbers ++; 
+	}
 }
 		
